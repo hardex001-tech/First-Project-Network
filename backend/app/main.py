c333023b43af2bc5a53c726e0fcba22964425abf
hardex001-tech <hardexmarley340@gmail.com>
2026-09-02 23:39:45 -0700

    fix: whitelist vercel domain in cors origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,10 +20,10 @@ prisma = Prisma()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "https://first-project-network.onrender.com"
-    ],
+    allow_origins = [
+    "http://localhost:3000",
+    "https://first-project-network-seven.vercel.app" 
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
